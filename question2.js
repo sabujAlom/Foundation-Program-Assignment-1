3c149c16e316fc694dc647dfa7f7c320d0989cec
sabujAlom <sabujalom18@gmail.com>
2026-09-02 12:26:29 +0600

    solve question 2

---
+++ question2.js
@@ -0,0 +1,26 @@
+function getDayType(day){
+    day = day.toLowerCase()
+    switch (day){
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        
+        default:
+            return "Invalid Day";
+        
+        
+    }
+}
+
+console.log(getDayType("Friday"));      
+console.log(getDayType("friday"));      
+console.log(getDayType("MONDAY"));      
+console.log(getDayType("Bandarban"));   
